b10a4f50591726a3bb2025634594e774e311dbd3
laurenyew <laurenyew@gmail.com>
2016-02-24 22:28:36 -0600

    My first commit

---
+++ app/src/main/java/laurenyew/photogalleryapp/PhotoGalleryActivityFragment.java
@@ -0,0 +1,22 @@
+package laurenyew.photogalleryapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PhotoGalleryActivityFragment extends Fragment {
+
+    public PhotoGalleryActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+    }
+}
