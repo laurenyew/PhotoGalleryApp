3113d1dabb3b9c8e0d5d79a649827fe4fbd6584b
laurenyew <laurenyew@gmail.com>
2016-03-27 09:22:21 -0500

    Adding Recyler View and the ability to switch between views

--- app/src/main/java/laurenyew/photogalleryapp/PhotoGalleryActivity.java
+++ app/src/main/java/laurenyew/photogalleryapp/PhotoGalleryActivity.java
@@ -1,5 +1,6 @@
 package laurenyew.photogalleryapp;
 
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,7 +16,11 @@ import laurenyew.photogalleryapp.detail.PhotoDetailFragment;
 
 public class PhotoGalleryActivity extends AppCompatActivity {
 
+    private enum VIEW_TYPE {
+        DETAIL, LIST, GALLERY
+    }
 
+    private VIEW_TYPE currentViewType = VIEW_TYPE.DETAIL;
 
     private ViewPager imageDetailPager;
 
@@ -29,6 +34,7 @@ public class PhotoGalleryActivity extends AppCompatActivity {
 
         if(savedInstanceState == null) {
 
+            currentViewType = VIEW_TYPE.DETAIL;
             imageDetailPager = (ViewPager) findViewById(R.id.image_detail_view_pager);
             imageDetailPagerAdapter = new PhotoDetailPagerAdapter(getSupportFragmentManager());
             imageDetailPager.setAdapter(imageDetailPagerAdapter);
@@ -48,6 +54,8 @@ public class PhotoGalleryActivity extends AppCompatActivity {
                             .setAction("Add a Photo", null).show();
                 }
             });
+
+            showViewType(currentViewType);
         }
 
     }
@@ -68,11 +76,51 @@ public class PhotoGalleryActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        //TODO: Modularize the views so that we don't have to load them unless they're being shown.
+
+        VIEW_TYPE oldViewType = currentViewType;
+        switch(id)
+        {
+            case R.id.action_gallery_page: break;
+            case R.id.action_list_page: {
+                System.out.println("Open List View");
+                currentViewType = VIEW_TYPE.LIST;
+                break;
+
+            }
+            case R.id.action_detail_page: {
+                System.out.println("Open Detail Pager");
+                currentViewType = VIEW_TYPE.DETAIL;
+                break;
+            }
+            case R.id.action_settings: break;
+            default: break;
+        }
+
+        if(currentViewType != oldViewType)
+        {
+            System.out.println("Changed view type to: " + currentViewType);
+            showViewType(currentViewType);
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Temporary helper method.
+     *
+     * Show/Hide other views accordingly.
+     *
+     * TODO: We will need to change this so that instead of hiding/showing
+     * views we can deal with the dffferent features in a more modular fashion.
+     *
+     * @param viewType
+     */
+    private void showViewType(VIEW_TYPE viewType)
+    {
+        System.out.println("Show view type: " + viewType);
+
+        findViewById(R.id.action_detail_page).setVisibility((VIEW_TYPE.DETAIL == currentViewType)? View.VISIBLE : View.INVISIBLE);
+        findViewById(R.id.action_list_page).setVisibility((VIEW_TYPE.LIST == currentViewType)? View.VISIBLE : View.INVISIBLE);
     }
 }
